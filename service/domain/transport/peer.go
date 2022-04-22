b1cd8692474022413475ed41b907740fdfc20da1
boreq <boreq@0x46.net>
2022-04-22 14:32:40 +0200

    Add comments and make the string easier to understand

--- service/domain/transport/peer.go
+++ service/domain/transport/peer.go
@@ -1,11 +1,17 @@
 package transport
 
 import (
+	"fmt"
+
 	"github.com/planetary-social/go-ssb/service/domain/identity"
 	"github.com/planetary-social/go-ssb/service/domain/refs"
 	"github.com/planetary-social/go-ssb/service/domain/transport/rpc"
 )
 
+// Peer is here just for the purpose of storing an RPC connection together with the identity of the remote node. In
+// theory that identity could be placed inside the rpc.Connection struct however at the protocol level the concept
+// of a remote identity exists only during the handshake, the RPC connection itself doesn't really know about the
+// handshake or the identity. Those are properties of the underlying boxstream transport layer.
 type Peer struct {
 	remote identity.Public
 	conn   *rpc.Connection
@@ -28,5 +34,5 @@ func (p Peer) Conn() *rpc.Connection {
 
 func (p Peer) String() string {
 	public, _ := refs.NewIdentityFromPublic(p.remote)
-	return public.String()
+	return fmt.Sprintf("<peer %s>", public.String())
 }

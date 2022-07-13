d13024a0eaa699b51b9e5c27fbf268b05a92f7ee
boreq <boreq@0x46.net>
2022-07-13 10:57:39 +0200

    tests

--- service/app/queries/published_log_test.go
+++ service/app/queries/published_log_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/planetary-social/scuttlego/di"
 	"github.com/planetary-social/scuttlego/fixtures"
 	"github.com/planetary-social/scuttlego/internal"
+	"github.com/planetary-social/scuttlego/service/adapters/mocks"
 	"github.com/planetary-social/scuttlego/service/app/queries"
 	"github.com/planetary-social/scuttlego/service/domain/refs"
 	"github.com/stretchr/testify/require"
@@ -15,6 +16,8 @@ func TestPublishedLog_NilStartSeqIsNotUsedToDetermineMessageSequence(t *testing.
 	app, err := di.BuildTestQueries()
 	require.NoError(t, err)
 
+	localFeed := refs.MustNewIdentityFromPublic(app.LocalIdentity).MainFeed()
+
 	query := queries.PublishedLog{
 		StartSeq: nil,
 	}
@@ -23,6 +26,17 @@ func TestPublishedLog_NilStartSeqIsNotUsedToDetermineMessageSequence(t *testing.
 	require.NoError(t, err)
 
 	require.Empty(t, app.ReceiveLogRepository.GetMessageCalls)
+	require.Equal(
+		t,
+		[]mocks.FeedRepositoryMockGetMessagesCall{
+			{
+				Id:    localFeed,
+				Seq:   nil,
+				Limit: nil,
+			},
+		},
+		app.FeedRepository.GetMessagesCalls,
+	)
 }
 
 func TestPublishedLog_NotNilStartSeqIsUsedToDetermineMessageSequence(t *testing.T) {
@@ -31,19 +45,31 @@ func TestPublishedLog_NotNilStartSeqIsUsedToDetermineMessageSequence(t *testing.
 
 	localFeed := refs.MustNewIdentityFromPublic(app.LocalIdentity).MainFeed()
 
-	seq := fixtures.SomeReceiveLogSequence()
-	msg := fixtures.SomeMessage(fixtures.SomeSequence(), localFeed)
+	receiveLogSequence := fixtures.SomeReceiveLogSequence()
+	sequence := fixtures.SomeSequence()
+	msg := fixtures.SomeMessage(sequence, localFeed)
 
 	query := queries.PublishedLog{
-		StartSeq: internal.Ptr(seq),
+		StartSeq: internal.Ptr(receiveLogSequence),
 	}
 
-	app.ReceiveLogRepository.MockMessage(seq, msg)
+	app.ReceiveLogRepository.MockMessage(receiveLogSequence, msg)
 
 	_, err = app.Queries.PublishedLog.Handle(query)
 	require.NoError(t, err)
 
 	require.NotEmpty(t, app.ReceiveLogRepository.GetMessageCalls)
+	require.Equal(
+		t,
+		[]mocks.FeedRepositoryMockGetMessagesCall{
+			{
+				Id:    localFeed,
+				Seq:   internal.Ptr(sequence),
+				Limit: nil,
+			},
+		},
+		app.FeedRepository.GetMessagesCalls,
+	)
 }
 
 func TestPublishedLog_StartSequenceMustPointToMessageFromMainLocalFeed(t *testing.T) {

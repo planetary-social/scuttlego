02956e971f0957dc1b909ff176f4db8a6c3d78f9
boreq <boreq@0x46.net>
2022-09-22 18:03:07 +0200

    Session

---
+++ service/adapters/ebt/ebt.go
@@ -0,0 +1,47 @@
+package ebt
+
+import (
+	"context"
+
+	"github.com/planetary-social/scuttlego/service/app/queries"
+	"github.com/planetary-social/scuttlego/service/domain/feeds/message"
+	"github.com/planetary-social/scuttlego/service/domain/refs"
+	"github.com/planetary-social/scuttlego/service/domain/replication/ebt"
+)
+
+type StreamMessagesRequestHandler struct {
+	handler queries.CreateHistoryStreamHandler
+}
+
+func NewStreamMessagesRequestHandler(handler queries.CreateHistoryStreamHandler) *StreamMessagesRequestHandler {
+	return &StreamMessagesRequestHandler{handler: handler}
+}
+
+func (h StreamMessagesRequestHandler) Handle(ctx context.Context, id refs.Feed, seq *message.Sequence, messageWriter ebt.MessageWriter) {
+	query := queries.CreateHistoryStream{
+		Id:             id,
+		Seq:            seq,
+		Limit:          nil,
+		Live:           true,
+		Old:            true,
+		ResponseWriter: NewMessageWriterAdapter(messageWriter),
+	}
+
+	h.handler.Handle(ctx, query)
+}
+
+type MessageWriterAdapter struct {
+	messageWriter ebt.MessageWriter
+}
+
+func NewMessageWriterAdapter(messageWriter ebt.MessageWriter) *MessageWriterAdapter {
+	return &MessageWriterAdapter{messageWriter: messageWriter}
+}
+
+func (m MessageWriterAdapter) WriteMessage(msg message.Message) error {
+	return m.messageWriter.WriteMessage(msg)
+}
+
+func (m MessageWriterAdapter) CloseWithError(err error) error {
+	return nil
+}
--- service/domain/replication/ebt/session.go
+++ service/domain/replication/ebt/session.go
@@ -8,10 +8,19 @@ import (
 	"github.com/planetary-social/scuttlego/logging"
 	"github.com/planetary-social/scuttlego/service/domain/feeds/message"
 	"github.com/planetary-social/scuttlego/service/domain/messages"
+	"github.com/planetary-social/scuttlego/service/domain/refs"
 )
 
 type RawMessageHandler interface {
 	Handle(msg message.RawMessage) error
+}
+
+type MessageWriter interface {
+	WriteMessage(msg message.Message) error
+}
+
+type MessageStreamer interface {
+	Handle(ctx context.Context, id refs.Feed, seq *message.Sequence, messageWriter MessageWriter)
 }
 
 type Stream interface {
@@ -63,41 +72,74 @@ func (i IncomingMessage) Err() error {
 }
 
 type SessionRunner struct {
-	remoteNotes map[string]messages.EbtReplicateNote
-	lock        sync.Mutex // guards remoteWants
-
 	logger            logging.Logger
 	rawMessageHandler RawMessageHandler
+	streamer          MessageStreamer
 }
 
-func NewSessionRunner() *SessionRunner {
-	return &SessionRunner{}
+func NewSessionRunner(
+	logger logging.Logger,
+	rawMessageHandler RawMessageHandler,
+	streamer MessageStreamer,
+) *SessionRunner {
+	return &SessionRunner{
+		logger:            logger,
+		rawMessageHandler: rawMessageHandler,
+		streamer:          streamer,
+	}
 }
 
 func (s *SessionRunner) HandleStream(ctx context.Context, stream Stream) error {
-	go s.handleIncomingMessages(ctx, stream)
+	session := NewSession(stream, s.logger, s.rawMessageHandler, s.streamer)
+	go session.handleIncomingMessages(ctx)
 
 	// todo send
 
 	return nil
 }
 
-func (s *SessionRunner) handleIncomingMessages(ctx context.Context, stream Stream) {
-	for incoming := range stream.IncomingMessages(ctx) {
-		if err := s.handleIncomingMessage(incoming); err != nil {
+type Session struct {
+	stream Stream
+
+	remoteNotes map[string]messages.EbtReplicateNote
+	lock        sync.Mutex // guards remoteNotes
+
+	logger            logging.Logger
+	rawMessageHandler RawMessageHandler
+	streamer          MessageStreamer
+}
+
+func NewSession(
+	stream Stream,
+	logger logging.Logger,
+	rawMessageHandler RawMessageHandler,
+	streamer MessageStreamer,
+) *Session {
+	return &Session{
+		stream: stream,
+
+		logger:            logger.New("session"),
+		rawMessageHandler: rawMessageHandler,
+		streamer:          streamer,
+	}
+}
+
+func (s *Session) handleIncomingMessages(ctx context.Context) {
+	for incoming := range s.stream.IncomingMessages(ctx) {
+		if err := s.handleIncomingMessage(ctx, incoming); err != nil {
 			s.logger.WithError(err).Debug("error processing incoming message")
 		}
 	}
 }
 
-func (s *SessionRunner) handleIncomingMessage(incoming IncomingMessage) error {
+func (s *Session) handleIncomingMessage(ctx context.Context, incoming IncomingMessage) error {
 	if err := incoming.Err(); err != nil {
 		return errors.Wrap(err, "error receiving messages")
 	}
 
 	notes, ok := incoming.Notes()
 	if ok {
-		return s.handleIncomingNotes(notes)
+		return s.handleIncomingNotes(ctx, notes)
 	}
 
 	msg, ok := incoming.Msg()
@@ -111,14 +153,47 @@ func (s *SessionRunner) handleIncomingMessage(incoming IncomingMessage) error {
 	return errors.New("logic error")
 }
 
-func (s *SessionRunner) handleIncomingNotes(notes messages.EbtReplicateNotes) error {
+func (s *Session) handleIncomingNotes(ctx context.Context, notes messages.EbtReplicateNotes) error {
 	s.lock.Lock()
 	defer s.lock.Unlock()
 
 	for _, note := range notes.Notes() {
 		s.remoteNotes[note.Ref().String()] = note
+
+		seq, err := s.parseSeq(note.Sequence())
+		if err != nil {
+			return errors.Wrap(err, "error parsing sequence")
+		}
+
+		// todo kill old streams
+		s.streamer.Handle(ctx, note.Ref(), seq, NewStreamMessageWriter(s.stream))
 	}
 
-	// todo initate sending messages
+	return nil
+}
+
+func (s *Session) parseSeq(seq int) (*message.Sequence, error) {
+	if seq <= 0 {
+		return nil, nil
+	}
+	sequence, err := message.NewSequence(seq)
+	if err != nil {
+		return nil, errors.Wrap(err, "new sequence error")
+	}
+	return &sequence, nil
+}
+
+type StreamMessageWriter struct {
+	stream Stream
+}
+
+func NewStreamMessageWriter(stream Stream) *StreamMessageWriter {
+	return &StreamMessageWriter{
+		stream: stream,
+	}
+}
+
+func (s StreamMessageWriter) WriteMessage(msg message.Message) error {
+	s.stream.SendMessage(&msg)
 	return nil
 }

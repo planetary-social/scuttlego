3713b66a61ce72256751e023b71bb19363881741
boreq <boreq@0x46.net>
2022-11-29 19:56:24 +0100

    iter

--- service/adapters/migrations/0001_import_data_from_gossb.go
+++ service/adapters/migrations/0001_import_data_from_gossb.go
@@ -6,7 +6,10 @@ import (
 
 	"github.com/boreq/errors"
 	"github.com/planetary-social/scuttlego/migrations"
+	"go.cryptoscope.co/luigi"
+	"go.cryptoscope.co/margaret"
 	"go.cryptoscope.co/ssb/sbot"
+	refs "go.mindeco.de/ssb-refs"
 )
 
 type MigrationImportDataFromGoSSB struct {
@@ -28,6 +31,49 @@ func (m MigrationImportDataFromGoSSB) Run(ctx context.Context, state migrations.
 	}
 
 	fmt.Println("seq", bot.ReceiveLog.Seq())
+
+	var lastSeq int64 = -1
+
+	src, err := bot.ReceiveLog.Query(
+		margaret.SeqWrap(true),
+		margaret.Gte(lastSeq),
+	)
+	if err != nil {
+		return state, errors.Wrap(err, "error querying receive log")
+	}
+
+	for {
+		v, err := src.Next(ctx)
+		if err != nil {
+			if luigi.IsEOS(err) {
+				break
+			}
+			return state, errors.Wrap(err, "error getting next value")
+		}
+
+		if err, ok := v.(error); ok {
+			if margaret.IsErrNulled(err) {
+				continue
+			}
+			return state, errors.Wrap(err, "margaret returned an error")
+		}
+
+		sw, ok := v.(margaret.SeqWrapper)
+		if !ok {
+			return state, fmt.Errorf("expected message seq wrapper but got '%T'", v)
+		}
+
+		rxLogSeq := sw.Seq()
+
+		msg, ok := sw.Value().(refs.Message)
+		if !ok {
+			return state, fmt.Errorf("expected message but got '%T'", sw.Value())
+		}
+
+		lastSeq = rxLogSeq
+
+		fmt.Println(rxLogSeq, msg.Key().String())
+	}
 
 	return nil, nil
 }

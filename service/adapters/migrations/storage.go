6aafad86cd4d3880635615ccf92eea626b7c2d35
boreq <boreq@0x46.net>
2022-12-01 21:30:09 +0100

    Implement statuses

--- service/adapters/migrations/storage.go
+++ service/adapters/migrations/storage.go
@@ -1,28 +1,124 @@
 package migrations
 
 import (
+	"github.com/boreq/errors"
 	"github.com/planetary-social/scuttlego/migrations"
+	"github.com/planetary-social/scuttlego/service/adapters/bolt/utils"
+	"go.etcd.io/bbolt"
+)
+
+const (
+	migrationsBucket       = "migrations"
+	migrationsBucketStatus = "status"
+	migrationsBucketState  = "state"
 )
 
 type BoltStorage struct {
+	db *bbolt.DB
 }
 
-func NewBoltStorage() *BoltStorage {
-	return &BoltStorage{}
+func NewBoltStorage(db *bbolt.DB) *BoltStorage {
+	return &BoltStorage{db: db}
 }
 
-func (b BoltStorage) LoadState(name string) (migrations.State, error) {
+func (b *BoltStorage) LoadState(name string) (migrations.State, error) {
 	return migrations.State{}, migrations.ErrStateNotFound
 }
 
-func (b BoltStorage) SaveState(name string, state migrations.State) error {
+func (b *BoltStorage) SaveState(name string, state migrations.State) error {
 	return nil
 }
 
-func (b BoltStorage) LoadStatus(name string) (migrations.Status, error) {
-	return migrations.Status{}, migrations.ErrStatusNotFound
+func (b *BoltStorage) LoadStatus(name string) (migrations.Status, error) {
+	var status migrations.Status
+
+	if err := b.db.View(func(tx *bbolt.Tx) error {
+		b, err := utils.GetBucket(tx, b.statusBucket())
+		if err != nil {
+			return errors.Wrap(err, "error creating bucket")
+		}
+
+		if b == nil {
+			return migrations.ErrStatusNotFound
+		}
+
+		v := b.Get([]byte(name))
+		if v == nil {
+			return migrations.ErrStatusNotFound
+		}
+
+		status, err = unmarshalStatus(string(v))
+		if err != nil {
+			return errors.Wrap(err, "error unmarshaling status")
+		}
+
+		return nil
+	}); err != nil {
+		if errors.Is(err, migrations.ErrStatusNotFound) {
+			return migrations.Status{}, err
+		}
+		return migrations.Status{}, errors.Wrap(err, "transaction failed")
+	}
+
+	return status, nil
 }
 
-func (b BoltStorage) SaveStatus(name string, status migrations.Status) error {
+func (b *BoltStorage) SaveStatus(name string, status migrations.Status) error {
+	marshaledStatus, err := marshalStatus(status)
+	if err != nil {
+		return errors.Wrap(err, "error marshaling status")
+	}
+
+	if err := b.db.Batch(func(tx *bbolt.Tx) error {
+		b, err := utils.CreateBucket(tx, b.statusBucket())
+		if err != nil {
+			return errors.Wrap(err, "error creating bucket")
+		}
+
+		return b.Put([]byte(name), []byte(marshaledStatus))
+	}); err != nil {
+		return errors.Wrap(err, "transaction failed")
+	}
 	return nil
+}
+
+func (b *BoltStorage) statusBucket() []utils.BucketName {
+	return []utils.BucketName{
+		utils.BucketName(migrationsBucket),
+		utils.BucketName(migrationsBucketStatus),
+	}
+}
+
+func (b *BoltStorage) stateBucket() []utils.BucketName {
+	return []utils.BucketName{
+		utils.BucketName(migrationsBucket),
+		utils.BucketName(migrationsBucketState),
+	}
+}
+
+const (
+	statusFailed   = "failed"
+	statusFinished = "finished"
+)
+
+func marshalStatus(status migrations.Status) (string, error) {
+	switch status {
+	case migrations.StatusFailed:
+		return statusFailed, nil
+	case migrations.StatusFinished:
+		return statusFinished, nil
+	default:
+		return "", errors.New("unknown status")
+	}
+}
+
+func unmarshalStatus(status string) (migrations.Status, error) {
+	switch status {
+	case statusFailed:
+		return migrations.StatusFailed, nil
+	case statusFinished:
+		return migrations.StatusFinished, nil
+	default:
+		return migrations.Status{}, errors.New("unknown status")
+	}
 }
---
+++ service/adapters/migrations/storage_test.go
@@ -0,0 +1,51 @@
+package migrations
+
+import (
+	"testing"
+
+	"github.com/planetary-social/scuttlego/fixtures"
+	"github.com/planetary-social/scuttlego/migrations"
+	"github.com/stretchr/testify/require"
+)
+
+func TestBoltStorage_SupportedStatusesAreSavedAndReturned(t *testing.T) {
+	testCases := []struct {
+		Name   string
+		Status migrations.Status
+	}{
+		{
+			Name:   "finished",
+			Status: migrations.StatusFinished,
+		},
+		{
+			Name:   "failed",
+			Status: migrations.StatusFailed,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.Name, func(t *testing.T) {
+			db := fixtures.Bolt(t)
+			storage := NewBoltStorage(db)
+
+			someName1 := fixtures.SomeString()
+			someName2 := fixtures.SomeString()
+
+			_, err := storage.LoadStatus(someName1)
+			require.ErrorIs(t, err, migrations.ErrStatusNotFound)
+
+			_, err = storage.LoadStatus(someName2)
+			require.ErrorIs(t, err, migrations.ErrStatusNotFound)
+
+			err = storage.SaveStatus(someName1, testCase.Status)
+			require.NoError(t, err)
+
+			status, err := storage.LoadStatus(someName1)
+			require.NoError(t, err)
+			require.Equal(t, status, testCase.Status)
+
+			_, err = storage.LoadStatus(someName2)
+			require.ErrorIs(t, err, migrations.ErrStatusNotFound)
+		})
+	}
+}

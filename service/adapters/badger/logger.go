8211b441f4f23d9d304bb4a3bb5064bccf9820f7
Filip Borkiewicz <boreq@0x46.net>
2023-01-11 16:37:18 +0000

    Make badger log level adjustable separately (#145)

--- service/adapters/badger/logger.go
+++ service/adapters/badger/logger.go
@@ -8,34 +8,52 @@ import (
 
 const originalLogLevelField = "original_level"
 
+type LoggerLevel int
+
+const (
+	LoggerLevelError LoggerLevel = iota
+	LoggerLevelWarning
+	LoggerLevelInfo
+	LoggerLevelDebug
+)
+
 type Logger struct {
 	logger logging.Logger
+	level  LoggerLevel
 }
 
-func NewLogger(logger logging.Logger) Logger {
-	return Logger{logger: logger}
+func NewLogger(logger logging.Logger, level LoggerLevel) Logger {
+	return Logger{logger: logger, level: level}
 }
 
 func (l Logger) Errorf(s string, i ...interface{}) {
-	l.logger.
-		WithField(originalLogLevelField, "error").
-		Error(fmt.Sprintf(s, i...))
+	if l.level >= LoggerLevelError {
+		l.logger.
+			WithField(originalLogLevelField, "error").
+			Error(fmt.Sprintf(s, i...))
+	}
 }
 
 func (l Logger) Warningf(s string, i ...interface{}) {
-	l.logger.
-		WithField(originalLogLevelField, "warning").
-		Error(fmt.Sprintf(s, i...))
+	if l.level >= LoggerLevelWarning {
+		l.logger.
+			WithField(originalLogLevelField, "warning").
+			Error(fmt.Sprintf(s, i...))
+	}
 }
 
 func (l Logger) Infof(s string, i ...interface{}) {
-	l.logger.
-		WithField(originalLogLevelField, "info").
-		Debug(fmt.Sprintf(s, i...))
+	if l.level >= LoggerLevelInfo {
+		l.logger.
+			WithField(originalLogLevelField, "info").
+			Debug(fmt.Sprintf(s, i...))
+	}
 }
 
 func (l Logger) Debugf(s string, i ...interface{}) {
-	l.logger.
-		WithField(originalLogLevelField, "debug").
-		Debug(fmt.Sprintf(s, i...))
+	if l.level >= LoggerLevelDebug {
+		l.logger.
+			WithField(originalLogLevelField, "debug").
+			Debug(fmt.Sprintf(s, i...))
+	}
 }

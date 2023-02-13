be39af46811e040b9ed5d569286c483bad3fcb93
Filip Borkiewicz <boreq@0x46.net>
2023-02-13 22:18:57 +0100

    Move item to separate file (#189)

--- service/adapters/badger/utils/bucket.go
+++ service/adapters/badger/utils/bucket.go
@@ -7,14 +7,6 @@ import (
 	"github.com/dgraph-io/badger/v3"
 )
 
-type Item interface {
-	KeyCopy([]byte) []byte
-	ValueCopy([]byte) ([]byte, error)
-
-	DangerousKey(func(key []byte) error) error
-	DangerousValue(func(value []byte) error) error
-}
-
 type Bucket struct {
 	tx     *badger.Txn
 	prefix Key
@@ -145,28 +137,4 @@ func (b Bucket) IsEmpty() bool {
 
 func (b Bucket) Prefix() Key {
 	return b.prefix
-}
-
-type itemAdapter struct {
-	item *badger.Item
-}
-
-func newItemAdapter(item *badger.Item) itemAdapter {
-	return itemAdapter{item: item}
-}
-
-func (i itemAdapter) KeyCopy(dst []byte) []byte {
-	return i.item.KeyCopy(dst)
-}
-
-func (i itemAdapter) ValueCopy(dst []byte) ([]byte, error) {
-	return i.item.ValueCopy(dst)
-}
-
-func (i itemAdapter) DangerousKey(f func(key []byte) error) error {
-	return f(i.item.Key())
-}
-
-func (i itemAdapter) DangerousValue(f func(value []byte) error) error {
-	return i.item.Value(f)
 }
--- service/adapters/badger/utils/item.go
+++ service/adapters/badger/utils/item.go
@@ -1,1 +1,35 @@
 package utils
+
+import "github.com/dgraph-io/badger/v3"
+
+type Item interface {
+	KeyCopy([]byte) []byte
+	ValueCopy([]byte) ([]byte, error)
+
+	DangerousKey(func(key []byte) error) error
+	DangerousValue(func(value []byte) error) error
+}
+
+type itemAdapter struct {
+	item *badger.Item
+}
+
+func newItemAdapter(item *badger.Item) itemAdapter {
+	return itemAdapter{item: item}
+}
+
+func (i itemAdapter) KeyCopy(dst []byte) []byte {
+	return i.item.KeyCopy(dst)
+}
+
+func (i itemAdapter) ValueCopy(dst []byte) ([]byte, error) {
+	return i.item.ValueCopy(dst)
+}
+
+func (i itemAdapter) DangerousKey(f func(key []byte) error) error {
+	return f(i.item.Key())
+}
+
+func (i itemAdapter) DangerousValue(f func(value []byte) error) error {
+	return i.item.Value(f)
+}

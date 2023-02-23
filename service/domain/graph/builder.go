b2cf795e9aebfada8627cda8fbca13545b76add0
boreq <boreq@0x46.net>
2023-02-23 17:26:57 +0100

    Fix social graph
    We were using depth first search for some reason, brain fart most
    likely.

--- service/domain/graph/builder.go
+++ service/domain/graph/builder.go
@@ -28,60 +28,116 @@ func (b *SocialGraphBuilder) Build(maxHops Hops, local refs.Identity) (SocialGra
 	g := SocialGraph{
 		graph: make(map[string]Hops),
 	}
-
 	localBlocks := internal.NewSet[string]()
-	currentHops := MustNewHops(0)
-	g.graph[local.String()] = currentHops
-	if err := b.depthFirstSearch(g, maxHops, currentHops, local, localBlocks); err != nil {
-		return g, errors.Wrap(err, "dfs failed")
+	queue := internal.NewQueue[feedWithDistance]()
+
+	g.graph[local.String()] = MustNewHops(0)
+	queue.Enqueue(feedWithDistance{
+		Feed: local,
+		Hops: MustNewHops(0),
+	})
+
+	for {
+		current, ok := queue.Dequeue()
+		if !ok {
+			break
+		}
+
+		childContacts, err := b.storage.GetContacts(current.Feed)
+		if err != nil {
+			return SocialGraph{}, errors.Wrap(err, "could not get contacts")
+		}
+
+		if current.Hops.Int() == 0 {
+			for _, childContact := range childContacts {
+				if childContact.Blocking() {
+					localBlocks.Put(childContact.Target().String())
+				}
+			}
+		}
+
+		for _, childContact := range childContacts {
+			childHops := MustNewHops(current.Hops.Int() + 1)
+
+			if childHops.Int() >= maxHops.Int()+1 {
+				continue
+			}
+
+			if _, visited := g.graph[childContact.Target().String()]; visited {
+				continue
+			}
+
+			isInBanList, err := b.banList.ContainsFeed(childContact.Target().MainFeed())
+			if err != nil {
+				return SocialGraph{}, errors.Wrap(err, "error checking the ban list")
+			}
+
+			if isInBanList || !childContact.Following() || childContact.Blocking() || localBlocks.Contains(childContact.Target().String()) {
+				continue
+			}
+
+			g.graph[childContact.Target().String()] = childHops
+			queue.Enqueue(feedWithDistance{Feed: childContact.Target(), Hops: childHops})
+		}
 	}
 
 	return g, nil
 }
 
-func (b *SocialGraphBuilder) depthFirstSearch(g SocialGraph, maxHops Hops, currentHops Hops, contact refs.Identity, localBlocks internal.Set[string]) error {
-	if currentHops.Int() >= maxHops.Int() {
-		return nil
-	}
+//func (b *SocialGraphBuilder) breadthFirstSearch(
+//	g SocialGraph,
+//	maxHops Hops,
+//	currentHops Hops,
+//	contact refs.Identity,
+//	localBlocks internal.Set[string],
+//) error {
+//	if currentHops.Int() >= maxHops.Int() {
+//		return nil
+//	}
+//
+//	childContacts, err := b.storage.GetContacts(contact)
+//	if err != nil {
+//		return errors.Wrap(err, "could not get contacts")
+//	}
+//
+//	if currentHops.Int() == 0 {
+//		for _, childContact := range childContacts {
+//			if childContact.Blocking() {
+//				localBlocks.Put(childContact.Target().String())
+//			}
+//		}
+//	}
+//
+//	currentHops = MustNewHops(currentHops.Int() + 1)
+//
+//	for _, childContact := range childContacts {
+//		isInBanList, err := b.banList.ContainsFeed(childContact.Target().MainFeed())
+//		if err != nil {
+//			return errors.Wrap(err, "error checking the ban list")
+//		}
+//
+//		if isInBanList || !childContact.Following() || childContact.Blocking() || localBlocks.Contains(childContact.Target().String()) {
+//			continue
+//		}
+//
+//		if existingHops, ok := g.graph[childContact.Target().String()]; ok {
+//			if existingHops.Int() > currentHops.Int() {
+//				g.graph[childContact.Target().String()] = currentHops
+//			}
+//			continue
+//		}
+//
+//		g.graph[childContact.Target().String()] = currentHops
+//
+//		if err := b.depthFirstSearch(g, maxHops, currentHops, childContact.Target(), localBlocks); err != nil {
+//			return errors.Wrap(err, "recursion failed")
+//		}
+//	}
+//
+//	return nil
+//}
 
-	childContacts, err := b.storage.GetContacts(contact)
-	if err != nil {
-		return errors.Wrap(err, "could not get contacts")
-	}
-
-	if currentHops.Int() == 0 {
-		for _, childContact := range childContacts {
-			if childContact.Blocking() {
-				localBlocks.Put(childContact.Target().String())
-			}
-		}
-	}
-
-	currentHops = MustNewHops(currentHops.Int() + 1)
-
-	for _, childContact := range childContacts {
-		isInBanList, err := b.banList.ContainsFeed(childContact.Target().MainFeed())
-		if err != nil {
-			return errors.Wrap(err, "error checking the ban list")
-		}
-
-		if isInBanList || !childContact.Following() || childContact.Blocking() || localBlocks.Contains(childContact.Target().String()) {
-			continue
-		}
-
-		if existingHops, ok := g.graph[childContact.Target().String()]; ok {
-			if existingHops.Int() > currentHops.Int() {
-				g.graph[childContact.Target().String()] = currentHops
-			}
-			continue
-		}
-
-		g.graph[childContact.Target().String()] = currentHops
-
-		if err := b.depthFirstSearch(g, maxHops, currentHops, childContact.Target(), localBlocks); err != nil {
-			return errors.Wrap(err, "recursion failed")
-		}
-	}
-
-	return nil
+type feedWithDistance struct {
+	Feed refs.Identity
+	Hops Hops
 }

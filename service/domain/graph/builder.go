d251c83e5e79ef27cf3b3a481a30d3c54f2b7275
boreq <boreq@0x46.net>
2023-02-23 18:17:20 +0100

    Remove commented out code

--- service/domain/graph/builder.go
+++ service/domain/graph/builder.go
@@ -84,59 +84,6 @@ func (b *SocialGraphBuilder) Build(maxHops Hops, local refs.Identity) (SocialGra
 	return g, nil
 }
 
-//func (b *SocialGraphBuilder) breadthFirstSearch(
-//	g SocialGraph,
-//	maxHops Hops,
-//	currentHops Hops,
-//	contact refs.Identity,
-//	localBlocks internal.Set[string],
-//) error {
-//	if currentHops.Int() >= maxHops.Int() {
-//		return nil
-//	}
-//
-//	childContacts, err := b.storage.GetContacts(contact)
-//	if err != nil {
-//		return errors.Wrap(err, "could not get contacts")
-//	}
-//
-//	if currentHops.Int() == 0 {
-//		for _, childContact := range childContacts {
-//			if childContact.Blocking() {
-//				localBlocks.Put(childContact.Target().String())
-//			}
-//		}
-//	}
-//
-//	currentHops = MustNewHops(currentHops.Int() + 1)
-//
-//	for _, childContact := range childContacts {
-//		isInBanList, err := b.banList.ContainsFeed(childContact.Target().MainFeed())
-//		if err != nil {
-//			return errors.Wrap(err, "error checking the ban list")
-//		}
-//
-//		if isInBanList || !childContact.Following() || childContact.Blocking() || localBlocks.Contains(childContact.Target().String()) {
-//			continue
-//		}
-//
-//		if existingHops, ok := g.graph[childContact.Target().String()]; ok {
-//			if existingHops.Int() > currentHops.Int() {
-//				g.graph[childContact.Target().String()] = currentHops
-//			}
-//			continue
-//		}
-//
-//		g.graph[childContact.Target().String()] = currentHops
-//
-//		if err := b.depthFirstSearch(g, maxHops, currentHops, childContact.Target(), localBlocks); err != nil {
-//			return errors.Wrap(err, "recursion failed")
-//		}
-//	}
-//
-//	return nil
-//}
-
 type feedWithDistance struct {
 	Feed refs.Identity
 	Hops Hops
